1f91ab14655cd21756db09825c1c73f7ca27eaf6
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-07-18 20:11:13 +0300

    AP_CYPHAL: add AP_Baro_CYPHAL

--- libraries/AP_CYPHAL/AP_CYPHAL_registers.cpp
+++ libraries/AP_CYPHAL/AP_CYPHAL_registers.cpp
@@ -65,6 +65,8 @@ static RegisterCell registers_table[CyphalRegisters::NUMBER_OF_REGISTERS] = {
     {"uavcan.sub.gps.status.id"},
     {"uavcan.sub.gps.pdop.id"},
     {"uavcan.sub.mag.id"},
+    {"uavcan.sub.baro.temp.id"},
+    {"uavcan.sub.baro.pres.id"},
 };
 
 
